b03f0da2b37fd7006e6a46d1dfa00ad797c494e2
sarathi-2003 <sarathip846@gmail.com>
2023-10-13 14:17:34 +0530

    adding multible components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-function App (){
-  const day = new Date();
+function ADD(){
   let a= 20;
   let b = 30;
   let c = a+b;
+  return(
+    <p>
+    {a} plus {b} = {c}
+  </p>  
+  )
+}
+
+function App (){
+  const day = new Date();
+  
   console.log(day);
   return(
    <div>
@@ -10,10 +19,14 @@ function App (){
    <div>
    <button onClick={print_the_page}>print</button>
    </div>
-   <p>
-     {a} plus {b} = {c}
-   </p>
-   </div>         
+
+   <ADD />
+   <ADD />
+   <ADD />
+   <ADD />
+   <ADD />
+   </div>       
+    
   )
   function print_the_page(){
     window.print()
