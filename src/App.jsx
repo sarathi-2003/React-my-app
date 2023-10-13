659b2600b32dcb88751dc1b5577adc213ff4e9ff
sarathi-2003 <sarathip846@gmail.com>
2023-10-13 13:31:21 +0530

    added the console element in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 function App (){
+  console.log('Hello from App component');
   return(
    <div>
     Hello world!
    <div>
    <button onClick={print_the_page}>print</button>
    </div>
-   </div>
+   </div>         
   )
   function print_the_page(){
     window.print()
