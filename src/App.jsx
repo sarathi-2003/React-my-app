85d0846fdb1d3421769ef69c5cd312be83ee82e4
sarathi-2003 <sarathip846@gmail.com>
2023-10-13 11:52:55 +0530

    add one button in top

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ function App (){
   return(
    <div>
     Hello world!
+   <div>
+   <button onClick={print_the_page}>print</button>
+   </div>
    </div>
   )
+  function print_the_page(){
+    window.print()
+  }
 }
 
 export default App;
