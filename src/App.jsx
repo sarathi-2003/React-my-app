9ea7efe17a7d98ac801162c275871fb2091353a6
sarathi-2003 <sarathip846@gmail.com>
2023-10-13 13:44:10 +0530

    added dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 function App (){
-  console.log('Hello from App component');
+  const day = new Date();
+  let a= 20;
+  let b = 30;
+  let c = a+b;
+  console.log(day);
   return(
    <div>
-    Hello world!
+    Hello world! Today time is {day.toString()}
    <div>
    <button onClick={print_the_page}>print</button>
    </div>
+   <p>
+     {a} plus {b} = {c}
+   </p>
    </div>         
   )
   function print_the_page(){
     window.print()
-    day = new Date();
+    
   }
   
 }
