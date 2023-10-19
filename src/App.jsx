0a8fb2cf7ef6f9b996a776cc7946c803bde89c9b
sarathi-2003 <sarathip846@gmail.com>
2023-10-19 21:51:08 +0530

    dashboard session task

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,126 @@
-function ADD(props){
-  // console.log(props)
-  let a= 20;
-  let b = 30;
-  let c = a+b;
-   
-  return(
-    // <p>{a} plus {b} = {c} </p> 
-     <div>
-      Hello { props.username }
+import React from "react";
+import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+function App(){
+  return (
+    <div>
+      <div id="wrapper">
+
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div className="sidebar-brand-icon rotate-n-15">
+            <i className="fas fa-laugh-wink"></i>
+        </div>
+        <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+    </a>
+
+    <hr className="sidebar-divider my-0"></hr>
+
+    <li className="nav-item active">
+        <a className="nav-link" href="index.html">
+            <i className="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <hr className="sidebar-divider"></hr>
+
+    <div className="sidebar-heading">
+        Interface
+    </div>
+
+    <li className="nav-item">
+        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Components</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div className="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Custom Components:</h6>
+                <a className="collapse-item" href="buttons.html">Buttons</a>
+                <a className="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+    <li className="nav-item">
+        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i className="fas fa-fw fa-wrench"></i>
+            <span>Utilities</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Custom Utilities:</h6>
+                <a className="collapse-item" href="utilities-color.html">Colors</a>
+                <a className="collapse-item" href="utilities-border.html">Borders</a>
+                <a className="collapse-item" href="utilities-animation.html">Animations</a>
+                <a className="collapse-item" href="utilities-other.html">Other</a>
+            </div>
+        </div>
+    </li>
+
+    <hr className="sidebar-divider"></hr>
+
+    <div className="sidebar-heading">
+        Addons
+    </div>
+
+    <li className="nav-item">
+        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i className="fas fa-fw fa-folder"></i>
+            <span>Pages</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div className="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Login Screens:</h6>
+                <a className="collapse-item" href="login.html">Login</a>
+                <a className="collapse-item" href="register.html">Register</a>
+                <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
+                <div className="collapse-divider"></div>
+                <h6 className="collapse-header">Other Pages:</h6>
+                <a className="collapse-item" href="404.html">404 Page</a>
+                <a className="collapse-item" href="blank.html">Blank Page</a>
+            </div>
+        </div>
+    </li>
+
+    <li className="nav-item">
+        <a className="nav-link" href="charts.html">
+            <i className="fas fa-fw fa-chart-area"></i>
+            <span>Charts</span></a>
+    </li>
+
+    <li className="nav-item">
+        <a className="nav-link" href="tables.html">
+            <i className="fas fa-fw fa-table"></i>
+            <span>Tables</span></a>
+    </li>
+
+    <hr className="sidebar-divider d-none d-md-block"></hr>
+
+    
+    <div className="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+    
+    <div className="sidebar-card d-none d-lg-flex">
+        <img className="sidebar-card-illustration mb-2" src="../src/assets/undraw_rocket.svg" alt="..."></img>
+        <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
+        <a className="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
+    </div>
+
+</ul>
+
+    </div>
     </div>
     
   )
 }
-
-function App (){
-  const day = new Date();
-  let user1 = 'sarathi';
-  let user2 = 'karuna';
-  console.log(day);
-  return(
-   <div>
-    Hello world! Today time is {day.toString()}
-   <div>
-   <button onClick={print_the_page}>print</button>
-   </div>
-   <ADD username ={user1} />
-   <ADD username ={user2} />
-   
-   </div>       
-    
-  )
-  function print_the_page(){
-    window.print()
-    
-  }
-  
-}
-
-export default App;
+export default App; 
